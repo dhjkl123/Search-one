8c0fc32d89c57f574218f19f749cf96d326beadd
ksw <dhjkl123@naver.com>
2023-07-28 15:23:52 +0900

    spring project init

---
+++ search-one/src/test/java/com/project/searchone/SearchOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.searchone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
