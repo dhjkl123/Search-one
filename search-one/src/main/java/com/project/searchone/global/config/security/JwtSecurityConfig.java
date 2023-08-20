2b33a2049c91640f2d39fcf114fd653dfec64466
dhjkl123 <dhjkl123@naver.com>
2023-08-20 22:08:23 +0900

    refactor : 경로 변경 및 파일명 수정

---
+++ search-one/src/main/java/com/project/searchone/global/config/security/JwtSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.project.searchone.global.config.security;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.project.searchone.global.config.security.application.JwtFilter;
+import com.project.searchone.global.config.security.application.TokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        // security 로직에 JwtFilter 등록
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/user/user2").permitAll()
+                        .anyRequest().authenticated()
+
+                )
+                .addFilterBefore(
+                new JwtFilter(tokenProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+    }
+}
---
+++ search-one/src/main/java/com/project/searchone/global/config/security/dto/LoginReqDto.java
@@ -0,0 +1,9 @@
+package com.project.searchone.global.config.security.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginReqDto {
+    private String id;
+    private String pw;
+}
