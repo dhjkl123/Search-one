ee8b25419db914a5eaecce7696328fe1d5069b46
dhjkl123 <dhjkl123@naver.com>
2023-08-22 00:42:50 +0900

    fix : 검색 로직 수정 (기능 테스트X)

--- search-one/src/main/java/com/project/searchone/domain/search/api/SearchController.java
+++ search-one/src/main/java/com/project/searchone/domain/search/api/SearchController.java
@@ -1,9 +1,9 @@
 package com.project.searchone.domain.search.api;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.Date;
+import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.cloud.FirestoreClient;
 import com.project.searchone.domain.board.dto.BoardResponseDto;
+import com.project.searchone.domain.search.dto.SearchResultResponseDto;
 
 @RestController
 @RequestMapping("/search")
@@ -29,26 +30,43 @@ public class SearchController {
 
     @GetMapping
     public ResponseEntity<?> findByKeyword(@RequestParam String query,
-                                      @RequestParam Integer count
+                                      @RequestParam Integer count,
+                                      @RequestParam String startDate, // 0001-01-01 ~
+                                      @RequestParam String endDate // ~ 9999-12-31
                                       ) throws InterruptedException, ExecutionException{
                                         
         Firestore db = FirestoreClient.getFirestore();
 
         List<BoardResponseDto> boards = new ArrayList<>();
 
+        Timestamp timestampStart = Timestamp.parseTimestamp(startDate + "T00:00:00+00:00"); 
+        Timestamp timestampEnd = Timestamp.parseTimestamp(endDate + "T00:00:00+00:00");
+
         CollectionReference  collectionReference = db.collection(COLLECTION_NAME);
+
         ApiFuture<QuerySnapshot> future = collectionReference
-                                            .whereGreaterThanOrEqualTo("content", query)
-                                            .limit(count.intValue())
+                                            .whereLessThanOrEqualTo("created_at", timestampEnd)
+                                            .whereGreaterThanOrEqualTo("created_at", timestampStart)
+                                            .orderBy("created_at")
                                             .get();
         
-        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
-        for (QueryDocumentSnapshot document : documents){
-            boards.add(document.toObject(BoardResponseDto.class));
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments()
+                                                            .stream().filter(doc-> {
+                                                                String content = doc.get("content").toString();
+                                                                String title = doc.get("title").toString();
+                                                                 return content.contains(query) || title.contains(query) ;
+                                                                } )
+                                                            .collect(Collectors.toList());
 
-        }
+        if(count.intValue() > documents.size())
+            count = documents.size();      
 
-        return new ResponseEntity<>(boards, HttpStatus.OK);
+        for (int i = 0 ; i < count.intValue() ; i++)
+            boards.add(documents.get(i).toObject(BoardResponseDto.class));
+        
+        SearchResultResponseDto result = new SearchResultResponseDto(documents.size(),boards);
+        
+        return new ResponseEntity<>(result, HttpStatus.OK);
 
     }
     
--- search-one/src/test/java/com/project/searchone/search/SearchTest.java
+++ search-one/src/test/java/com/project/searchone/search/SearchTest.java
@@ -3,15 +3,20 @@ package com.project.searchone.search;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import org.assertj.core.api.filter.FilterOperator;
+import org.hibernate.sql.ast.tree.predicate.FilterPredicate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.Date;
+import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
@@ -21,6 +26,7 @@ import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.firestore.v1.StructuredQuery.Filter;
 
 @SpringBootTest
 public class SearchTest {
@@ -49,26 +55,33 @@ public class SearchTest {
 
 
     CollectionReference  collectionReference = db.collection(COLLECTION_NAME);
-    
-        Date dateStart = Date.parseDate("0001-01-01");
-        Date dateEnd = Date.parseDate("9999-12-31");
 
-        ApiFuture<QuerySnapshot> future = collectionReference
-                                            .whereLessThanOrEqualTo("content", "발생")
-                                            //.startAfter(lastVisible)
-                                      
-                                           
 
-                                            .limit(1)
-                                            .get();
+
+    Timestamp timestampStart = Timestamp.parseTimestamp("2023-08-13T00:00:00+00:00");
+    Timestamp timestampEnd = Timestamp.parseTimestamp("2023-08-20T00:00:00+00:00");
+
+    ApiFuture<QuerySnapshot> future = collectionReference
+                                        //.whereLessThanOrEqualTo("content", "발생")
+                                        .whereLessThanOrEqualTo("created_at", timestampEnd)
+                                        .whereGreaterThanOrEqualTo("created_at", timestampStart)
+                                        .orderBy("created_at")
+                                        //.startAt(timestampStart)
+                                        
+                                        //.limit(3)
+                                        .get();
     
-    List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+    List<QueryDocumentSnapshot> documents = future.get().getDocuments()
+                                            .stream().filter(doc-> doc.get("content").toString().contains("발생"))
+                                            .collect(Collectors.toList());
+
+    
     for (QueryDocumentSnapshot document : documents){
         //list.add(document.toObject(BoardResponseDto.class));
         result.add(document.get("title").toString());
     }
 
-    lastVisible = documents.get(documents.size()-1);
+    //lastVisible = documents.get(documents.size()-1);
 
 
 
