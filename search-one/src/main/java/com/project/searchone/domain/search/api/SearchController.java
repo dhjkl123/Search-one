bb8a2fa4a71c65b5b10282ecf19178fee7c0b521
dhjkl123 <dhjkl123@naver.com>
2023-08-25 23:07:15 +0900

    refactor : import 제거

--- search-one/src/main/java/com/project/searchone/domain/search/api/SearchController.java
+++ search-one/src/main/java/com/project/searchone/domain/search/api/SearchController.java
@@ -1,9 +1,5 @@
 package com.project.searchone.domain.search.api;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.api.core.ApiFuture;
-import com.google.cloud.Timestamp;
-import com.google.cloud.firestore.CollectionReference;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
-import com.google.firebase.cloud.FirestoreClient;
-import com.project.searchone.domain.board.dto.BoardResponseDto;
 import com.project.searchone.domain.search.dao.SearchDao;
 import com.project.searchone.domain.search.dto.SearchResultResponseDto;
 
